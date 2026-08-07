e399ea2b22f1bde01b39141d30c2585a7f6d7c47
Rishu Sharma <sharmarishu2006.23@gmail.com>
2026-08-07 10:51:28 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -105,7 +105,7 @@ export const login = async (req,res) => {
             profile:user.profile,
         }
 
-        return res.status(200).cookie("token",token, {maxAge:1*24*60*60*1000 , httpsOnly:true , sameSite:'strict' , secure:true}).json({
+        return res.status(200).cookie("token",token, {maxAge:1*24*60*60*1000 , httpsOnly:true , sameSite:'strict' , secure:true,}).json({
             message: `Welcome Back ${user.fullname}`,
             user,
             success:true,
