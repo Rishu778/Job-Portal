edb4b7d07e5ecdaeac0ed91788c93c8fa41f1700
Rishu Sharma <sharmarishu2006.23@gmail.com>
2026-08-07 10:57:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
     origin:[
-        'https://job-portal-rho-snowy.vercel.app/',
+        'http://localhost:5173',
+        'https://job-portal-rho-snowy.vercel.app',
         ]
     credentials:true
 }
